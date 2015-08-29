999c30110f03d8b9943a94ac2b29a8857f2e0cfe
serj@053 <serj053@yandex.ru>
2015-08-29 17:45:57 +0300

    создание таблицы под списком устройств

--- public_html/index.php
+++ public_html/index.php
@@ -1,10 +1,13 @@
 <?php
-/*проверяем масив GET на наличие вызовов объект/метод
-*Fan/default - загрузка страницы по умолчанию,
+/* index.php - точка входа, распределение по классам*/
+/*Через ссылки в массив GET[] передаются пары значений Класс/метод.
+ * Проверяем массив GET на наличие вызовов Класс/метод
+*List/default - загрузка страницы-списка по умолчанию,
 *Journal/(evetn, -alarm, -logg, -all) - загрузка соотвтетствующей страницы журнала
 */
-//echo 'IN index<br>';die;
-//var_dump($_GET);
+
+
+/*Автозагрузка классов*/
 	function __autoload($class){
                         $directories = array('controller/','model/');
                         foreach($directories as $directory){
@@ -13,26 +16,17 @@
                                         require $file;
                                         return;
                                 }
-                        }
-		
+                        }		
 	}
 
-	$obj = isset($_GET['obj'])?$_GET['obj']:'List/load';
+/*Выделяем из элементов массива GET[] имена классов и методов*/
+	$obj = isset($_GET['obj'])?$_GET['obj']:'List/load';       
 	$obj = explode('/',$obj);
 
 	$act = $obj[1];
 	$obj = $obj[0];
-/*		
-if($obj == 'Journal'){
-	echo ' IN INDEX - $obj / $act = '.$obj.'/'.$act.'<br>';
-	if(isset($_GET['str'])&& isset($_GET['fan_num'])){
-	echo $_GET['str'].' - '.$_GET['fan_num'];
-	}
-}	
-*/
-  // echo '$obj = '.$obj.'<br>';
- // echo '$act = '.$act.'<br>';die;
-	
+        
+/*ВЫзов соответствующего Класс/метод*/
 	$obj = $obj."Controller";
 
 	$obj = new $obj;
