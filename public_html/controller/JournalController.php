8b9a0527612ccc6bea3464a7938b0e1da307b150
serj@053 <serj053@yandex.ru>
2015-08-29 18:38:01 +0300

    создание таблицы под списком устройств

--- public_html/controller/JournalController.php
+++ public_html/controller/JournalController.php
@@ -1,105 +1,111 @@
 <?php 
-/*записывает состояние установки в журнал*/
+/*Класс записывает состояние установки в журнал*/
+
+/*Добавляем код класса для работы с базой данных*/
 require 'model/DB_connect.php';
-//echo'In jornalCobntroller()';	
+
 class JournalController{
-		
-                public function action($action){
+        /*Метод параметром которого является имя меотода , порверяет наличие
+         *  данного метода в классе, если наличие подтверждается то происходит его выполнение*/
+        public function action($action){
 
-                        $method = $action."Action";
-                        if(method_exists($this, $method)){
-                                $this->$method();
-                        }			
-                }
+                $method = $action."Action";
+                if(method_exists($this, $method)){
+                        $this->$method();
+                }			
+        }
+        
+/*заносим в базу данных данные и действия*/
+    private function addAction(){
 
-        /*1*/		private function addAction(){
-        /*заносим в базу данных данные и действия */
-        //echo 'In addAction()';
+        /*Выбираем закладку в которой будут происходить изменения*/
                 $choose = $_GET['id_table'];
-                $obj = new DB_connect;	
-                $obj->insertMsg();//вставляем в базу	
-                $choose = $choose.'Action';	
-        //echo 'choose = '.$choose;		die;		
+        /*Соеденение с базой*/        
+                $obj = new DB_connect;
+        /*Заносим в базу жанные , которые беруться из массива GET[] в моделе*/        
+                $obj->insertMsg();
+        /*Формируем имя метода для обработки данных для конкретной закладки журнала*/        
+                $choose = $choose.'Action';			
                 $this->$choose();		
                 }	
-
-        /*2*/			private function delAction(){
-        /*удаляем из базы данных данные */
-        //echo 'in GET[id] - '.$_GET['id'];
-                $choose = $_GET['id_table'];//активная вкладка
-                $id = $_GET['id'];	
-                $obj = new DB_connect;		
-                $ms = $obj->deleteMsg($id);		
-                $choose = $choose.'Action';
-        //echo 'choose = '.$choose;		die;		
+/*Удаление из базы выбранной записи(строки)*/
+    private function delAction(){
+      
+        /*Имя вкладки из которой будет удалена запись*/
+                $choose = $_GET['id_table'];
+        /*Идентификатор по которму определяется удаляемая строка*/        
+                $id = $_GET['id'];
+        /*Соеденение с базой*/        
+                $obj = new DB_connect;
+        /*Удаление выбранной записи, */        
+                $ms = $obj->deleteMsg($id);
+        /*Фрмируе имя метода для конкретной вкладки*/        
+                $choose = $choose.'Action';	
                 $this->$choose();				
                 }	
-/*3*/		
-		private function allAction(){
-/*получаем из базы данных ВСЕ данные и вставляем в таблицу*/
-//echo 'In allaction()';
-                        $obj = new DB_connect;	
-                        $ms = $obj->selectAllMsg();	
-//	var_dump($ms);		
-                        ob_start();
-                        require 'view/journal_all.php';
-                        ob_end_flush();
-		}
-		
-/*4*/	private function alarmAction(){
+
+/*Получаем из базы данные для вкладки "ВСЕ"*/
+        private function allAction(){
+
+                $obj = new DB_connect;	
+                $ms = $obj->selectAllMsg();		
+                ob_start();
+                require 'view/journal_all.php';
+                ob_end_flush();
+             }
+                
 /*нажатие вкладки ПРЕДУПРЕЖДЕНИЕ
-получаем из базы данных "ПРЕДУПРЕЖДЕНИЯ" и вставляем в таблицу
-выбираем из базы по набору идентификаторов*/
+*получаем из базы данных "ПРЕДУПРЕЖДЕНИЯ" и вставляем в таблицу
+*выбираем из базы по набору идентификаторов*/		
+        private function alarmAction(){
 
-                        $arr_id= array('FAIL_FAN_BTN','FAIL_F_BTN','FAIL_FREEZ_BTN','FAIL_VALVE_BTN','RESET');
-                        $obj = new DB_connect;
-                        $ms = $obj->selectSomeMsg($arr_id);	
-            //var_dump($ms);		
-                                ob_start();
-                                 require'view/journal_alarm.php';
-                                //$journal = ob_get_contents();
-                        ob_end_flush();
-                        }
+            /*Массив идентификаторов в соответствии с которыми будут выбираться строки из
+            базы (идентификаторы аварийных кнопок) */
+            $arr_id= array('FAIL_FAN_BTN','FAIL_F_BTN','FAIL_FREEZ_BTN','FAIL_VALVE_BTN','RESET');
+            $obj = new DB_connect;
+            $ms = $obj->selectSomeMsg($arr_id);			
+            ob_start();
+            require'view/journal_alarm.php';
+            ob_end_flush();
+          }
 		
-		
-		
-/*5*/	private function logAction(){
 /*нажатие вкладки ЖУРНАЛ ОПЕРАЦИЙ
-получаем из базы данных "ОПЕРАЦИИ" и вставляем в таблицу*/
-                        $arr_id= array('FAIL_FAN_BTN','FAIL_F_BTN','FAIL_FREEZ_BTN','FAIL_VALVE_BTN','RESET');
-                        //$arr_id = 'operation';
-                        $obj = new DB_connect;
-                        $operation_ms = $obj->selectSomeMsg($arr_id,'operation');	
-                                ob_start();
-                                 require'view/journal_log.php';
-                        ob_end_flush();
-                        }
-		
-		
-/*6*/	private function eventAction(){
-/*нажатие вкладки ЖУРНАЛ ОПЕРАЦИЙ
-получаем из базы данных "ОПЕРАЦИИ" и вставляем в таблицу*/
-                         $arr_id= array('FAIL_FAN_BTN','FAIL_F_BTN','FAIL_FREEZ_BTN','FAIL_VALVE_BTN','RESET');
-                        $obj = new DB_connect;
-                        $event_ms = $obj->selectSomeMsg($arr_id,'event');							
-                                ob_start();
-                                 require'view/journal_event.php';
-                        ob_end_flush();
-                        }
+*получаем из базы данных "ОПЕРАЦИИ" и вставляем в таблицу*/
+        private function logAction(){
 
-               private function checkedAction(){
-            /*передаем в таблицу по "id" строки отмеченны как  checked*/
-                /*заносим в базу данных данные действие */
-            //echo 'In selecktAction()';
-                        $choose = $_GET['id_table'];
-                        $obj = new DB_connect;	
-                        $obj->updateChecked();//вставляем в базу	
-                        $choose = $choose.'Action';	
-            //echo 'choose = '.$choose;		die;		
-                        $this->$choose();
-	}
+            $arr_id= array('FAIL_FAN_BTN','FAIL_F_BTN','FAIL_FREEZ_BTN','FAIL_VALVE_BTN','RESET');
+            //$arr_id = 'operation';
+            $obj = new DB_connect;
+            $operation_ms = $obj->selectSomeMsg($arr_id,'operation');	
+            ob_start();
+            require'view/journal_log.php';
+            ob_end_flush();
+            }
 		
-		
+/*нажатие вкладки ЖУРНАЛ ОПЕРАЦИЙ
+*получаем из базы данных "ОПЕРАЦИИ" и вставляем в таблицу*/		
+    private function eventAction(){
+
+            $arr_id= array('FAIL_FAN_BTN','FAIL_F_BTN','FAIL_FREEZ_BTN','FAIL_VALVE_BTN','RESET');
+            $obj = new DB_connect;
+            $event_ms = $obj->selectSomeMsg($arr_id,'event');							
+            ob_start();
+            require'view/journal_event.php';
+            ob_end_flush();
+           }
+           
+ /*передаем в таблицу по "id" строки отмеченны как  checked и выносим изменения
+*на страницу  */
+     private function checkedAction(){
+           
+            $choose = $_GET['id_table'];
+            $obj = new DB_connect;	
+            $obj->updateChecked();//вставляем в базу	
+            $choose = $choose.'Action';		
+            $this->$choose();
+}
+
+
 }
 	
 	
